ae0cc8fa3ca1f503f198cee20d4500caef53c8ae
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-09 21:04:21 +0200

    first commit, initialize react-app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, {FunctionComponent, useState}  from 'react';
+  
+const App: FunctionComponent = () => {
+const [name, setName] = useState<String>('React');    
+ return (
+  <h1>Wesh, {name} !</h1>
+ )
+}
+  
+export default App;
