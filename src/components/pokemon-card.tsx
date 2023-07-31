fe885681b7f424736a898c6273e0bc2a56feb9b3
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-31 12:50:43 +0200

    create folder helpers with function formatDate and formatType and import on pokemon-card  to display on app

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import "./pokemon-card.css";
+import formatDate from '../helpers/format-date';
+import formatType from '../helpers/format-type';
   
 type Props = {
   pokemon: Pokemon
@@ -19,7 +21,7 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
     const hideBorder = () => {
         setColor('#F5F5F5');
     }
-
+    
   return (
     <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
       <div className="card horizontal" style={{borderColor : color}}>
@@ -29,7 +31,10 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
+            {pokemon.types.map(type => (
+                <span key={type} className={formatType(type)}>{type}</span>
+            ))}
           </div>
         </div>
       </div> 
