ea1f8a306179b3d8a64bf73d0d65ebb733f683c0
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-31 10:39:18 +0200

    feat: display pokemons and style with materialize

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,26 @@ useEffect(() => {
 },[]);
  return (
   <div>
-    <h1>Pokédex</h1>
+    <h1 className="center">Pokédex</h1>
+    <div className="container">
+      <div className="row">
+        {pokemons.map(({id, name, picture, created}) =>(
+          <div className="col s6 m4" key={id}>
+            <div className="card honrizontal">
+              <div className="card-image">
+                <img src={picture} alt={name} />
+              </div>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p>{name}</p>
+                  <p><small>{created.toString()}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
     <p>Il y a {pokemons.length} pokémons dans mon pokédex.</p>
   </div> 
   )
